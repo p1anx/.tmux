0325a5b9d2802648287da02a54cd308b65d72da8
tuf-u24 <2514034568@qq.com>
2024-12-15 15:42:04 +0800

    update tmux

--- tmux.sh
+++ tmux.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
-mkdir -p "$HOME"/oh_my_tmux
 OH_MY_TMUX_DIR="$HOME/oh_my_tmux"
-git clone https://github.com/p1anx/.tmux.git $OH_MY_TMUX_DIR
+if [ -d "$OH_MY_TMUX_DIR" ]; then
+  rm -rf $OH_MY_TMUX_DIR
+fi
+if [ -f "$HOME/.tmux.conf" ] && [ -f "$HOME/.tmux.conf.local" ]; then
+  rm -rf .tmux.*
+fi
+if [ -d "$HOME/.config/tmux" ]; then
+  rm -rf $HOME/.config/tmux
+fi
+mkdir -p "$HOME"/oh_my_tmux
 mkdir -p ~/.config/tmux
+
+git clone https://github.com/p1anx/.tmux.git $OH_MY_TMUX_DIR
 ln -s $OH_MY_TMUX_DIR/.tmux.conf ~/.config/tmux/tmux.conf
 cp $OH_MY_TMUX_DIR/.tmux.conf.local ~/.config/tmux/tmux.conf.local
+echo "tmux is OK"
